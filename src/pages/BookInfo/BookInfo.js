125911db4672e4b1225a96ef5c32a4a95decf734
cocacollllla <cocacollllla@gmail.com>
2022-11-16 20:35:50 +0900

    recording폴더삭제, home안에 list, myrecord삭제, bookinfo수정, search수정

--- src/pages/BookInfo/BookInfo.js
+++ src/pages/BookInfo/BookInfo.js
@@ -1,73 +1,109 @@
-import React, {useState, useEffect} from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import { bookApi } from '../../api/axios';
-import BookInfoTop from '../../components/BookInfoTop';
-import styled from 'styled-components';
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { bookApi, getBook } from "../../api/axios";
+import BookInfoTop from "../../components/BookInfoTop";
+
+import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 const BookInfo = () => {
   const [bookInfo, setBookInfo] = useState({});
-  const navigate = useNavigate();
+  const [myBook, setMyBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const users = useSelector((state) => state.users);
   const params = useParams();
+  const navigate = useNavigate();
 
-  const isbn = params.isbn;
+  const id = params.id;
 
   useEffect(() => {
     const getBookInfo = async () => {
-      const book = await bookApi.info(isbn);
+      const book = await bookApi.info(id);
+      const mybook = await getBook(users.uid, id);
       setBookInfo(...book.data.item);
+      setMyBook(mybook);
+      setLoading(false);
     };
     getBookInfo();
-  }, [isbn]);
+  }, [id, users]);
+
+  console.log(loading);
+
+  const bookInfoObj = {
+    author: bookInfo.author,
+    title: bookInfo.title,
+    cover: bookInfo.cover,
+    publisher: bookInfo.publisher,
+    id: bookInfo.id,
+  };
 
   return (
     <BookInfoWrap>
-
-        
-        {Object.keys(bookInfo).length !== 0 &&
+      {Object.keys(bookInfo).length !== 0 && (
         <Box>
           <BookInfoBox>
-            <BookInfoTop bookInfo={bookInfo} />
+            <BookInfoTop bookInfo={bookInfoObj} />
 
             <InfoBottom>
-              <Category><div>분류</div><div>{bookInfo.categoryName}</div></Category>
-              <Description><div>책 소개</div><div>{bookInfo.description}</div></Description>
+              <Category>
+                <div>분류</div>
+                <div>{bookInfo.categoryName}</div>
+              </Category>
+              <Description>
+                <div>책 소개</div>
+                <div>{bookInfo.description}</div>
+              </Description>
             </InfoBottom>
-
-            
           </BookInfoBox>
-          <div className="btn uploadBtn" onClick={() => navigate(`/recording/write/${isbn}`)}>독서 기록하기</div>
+          <div
+            className="btn uploadBtn"
+            onClick={() =>
+              myBook.length !== 0
+                ? navigate(`/view/${myBook[0].docId}`)
+                : navigate(`/write/${id}`, {
+                    state: {
+                      bookInfo: {
+                        id,
+                        author: bookInfo.author,
+                        cover: bookInfo.cover,
+                        title: bookInfo.title,
+                        publisher: bookInfo.publisher,
+                      },
+                    },
+                  })
+            }
+          >
+            {myBook.length !== 0 ? "내 기록 보기" : "독서 기록하기"}
+          </div>
         </Box>
-        }
-      
-    
+      )}
+      {/* <HeaderRight icon="write" /> */}
     </BookInfoWrap>
-  )
-}
+  );
+};
 
 export default BookInfo;
 
 const BookInfoWrap = styled.div`
-  height: 100vh;
-  padding: 80px 20px 20px 20px;
+  height: 100%;
+  padding-bottom: 20px;
 `;
 
 const Box = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 100%;
 `;
 
 const BookInfoBox = styled.div`
-  padding: 10px 7px;
-  border: 1px solid ${(props) => props.theme.borderColor};
+  // border: 1px solid ${(props) => props.theme.borderColor};
   border-radius: 5px;
 `;
 
 const InfoBottom = styled.div`
   font-size: 0.9rem;
 `;
-
 
 const Category = styled.div`
   margin-bottom: 30px;
--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import Loading from "../../components/Loading";
+import { useLocation, useNavigate } from "react-router-dom";
+// import Loading from "../../components/Loading";
 import Target from "./Target";
 import { BsArrowUpCircleFill } from "react-icons/bs";
 import { bookApi } from "../../api/axios";
 import styled from "styled-components";
-import ListResult from "../../components/ListResult";
+import Loading from "../../components/Loading";
+// import ListResult from "../../components/ListResult";
 
 const Search = () => {
+  // const [searchKeyword, setSearchKeyword] = useState("");
+  // const [searchKeyword2, setSearchKeyword2] = useState(() => {
+  //   return window.sessionStorage.getItem("keyword") || "";
+  // });
+  // const [searchKeyword2, setSearchKeyword2] = useState("");
   const [searchResultList, setSearchResultList] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const [totalResult, setTotalResult] = useState(null);
 
   const location = useLocation();
+  const navigate = useNavigate();
   const keyword = location.state;
+
+  console.log(location);
 
   useEffect(() => {
     const getData = async () => {
@@ -22,6 +31,7 @@ const Search = () => {
       const result = await bookApi.search(keyword, page);
       if (page === 1) {
         setSearchResultList(result.data.item);
+        console.log(result);
         window.scrollTo(0, 0);
       } else {
         setSearchResultList((prev) => [...prev, ...result.data.item]);
@@ -41,41 +51,95 @@ const Search = () => {
     setSearchResultList([]);
   }, [keyword]);
 
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setLoading(true);
+  //     const result = await bookApi.search(keyword, page);
+  //     if (page === 1) {
+  //       setSearchResultList(result.data.item);
+  //       window.scrollTo(0, 0);
+  //     } else {
+  //       setSearchResultList((prev) => [...prev, ...result.data.item]);
+  //     }
+  //     setTotalResult(result.data.totalResults);
+  //     setLoading(false);
+  //   };
+
+  //   if (keyword !== null) {
+  //     getData();
+  //   }
+  // }, [page, keyword]);
+
+  // useEffect(() => {
+  //   setPage(1);
+  //   setTotalResult(null);
+  //   setSearchResultList([]);
+  // }, [keyword]);
+
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  const bookInfoClickHandler = (id) => {
+    navigate(`/bookinfo/${id}`);
+  };
+
+  // if (loading) {
+  //   return <Loading />;
+  // }
+  console.log(loading);
   return (
-    <SearchWrap>
+    <>
       {page === 1 && loading ? (
-        <Loading full={true} />
+        <Loading />
       ) : (
-        <>
-          <BookList keyword={keyword === null || searchResultList.length === 0}>
-            <ListResult bookList={searchResultList} />
+        <SearchWrap>
+          {/* <SearchBar /> */}
+          {/* <SearchBar>
+        <input
+          type="text"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+          onKeyPress={handleOnKeyPress}
+          autoFocus
+        />
+      </SearchBar> */}
+
+          <BookList>
+            {/* <ListResult bookList={searchResultList} /> */}
+            {searchResultList !== undefined &&
+              searchResultList.map((book) => (
+                <Book
+                  key={book.isbn}
+                  onClick={() => bookInfoClickHandler(book.isbn)}
+                >
+                  {book.title}
+                </Book>
+              ))}
             {page >= 2 && (
               <BsArrowUpCircleFill className="topBtn" onClick={scrollToTop} />
             )}
           </BookList>
-          {totalResult !== searchResultList.length &&
+          {searchResultList !== undefined &&
+            totalResult !== searchResultList.length &&
             searchResultList.length !== 0 && (
               <Target loading={loading} setPage={setPage} />
             )}
-        </>
+        </SearchWrap>
       )}
-    </SearchWrap>
+    </>
   );
 };
 
 export default Search;
 
 const SearchWrap = styled.div`
-  padding: 80px 20px;
+  padding: 10px 0;
 `;
 
 const BookList = styled.div`
   height: ${(props) => (props.keyword ? "100vh" : "auto")};
-
+  margin-top: 10px;
   .topBtn {
     position: fixed;
     bottom: 20px;
@@ -83,4 +147,8 @@ const BookList = styled.div`
     font-size: 2rem;
     color: ${(props) => props.theme.mainColor};
   }
+`;
+
+const Book = styled.div`
+  padding: 50px 0;
 `;
