7262389e5589165af74a21491ea2c09b95edd794
cocacollllla <cocacollllla@gmail.com>
2022-12-01 18:55:01 +0900

    비율 선택 테스트

--- src/pages/View/BookmarkSave.js
+++ src/pages/View/BookmarkSave.js
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 import { toPng } from "html-to-image";
 import { MdClose, MdOutlineSaveAlt } from "react-icons/md";
 import styled from "styled-components";
 
 const BookmarkSave = ({ bookmarkSaveClose, bookmark }) => {
+  const [ratio, setRatio] = useState("ratio1");
   const printRef = useRef();
 
   const handleDownloadImage = async () => {
@@ -39,6 +40,8 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark }) => {
     //   });
   };
 
+  console.log(ratio);
+
   return (
     <Container>
       <ButtonContainer>
@@ -51,17 +54,25 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark }) => {
       </ButtonContainer>
 
       <ViewContainer>
-        <Box ref={printRef}>
-          <div>{bookmark.text}</div>
+        <Box>
+          <ScreenShotBox ref={printRef} ratio={ratio}>
+            <p>{bookmark.text}</p>
+          </ScreenShotBox>
+
           {/* <div>
             <img src="/assets/2323.jpeg" alt="이미지" className="imageSize" />
           </div> */}
         </Box>
       </ViewContainer>
 
-      {/* <EditContainer>
-
-      </EditContainer> */}
+      <EditContainer>
+        <div className="ratio list">
+          <ul>
+            <li onClick={() => setRatio("ratio1")}>1 : 1</li>
+            <li onClick={() => setRatio("ratio2")}>3 : 4</li>
+          </ul>
+        </div>
+      </EditContainer>
     </Container>
   );
 };
@@ -78,34 +89,55 @@ const Container = styled.div`
   background-color: ${(props) => props.theme.black};
 `;
 
-const ViewContainer = styled.div``;
-
-const Box = styled.div`
-  background-image: url("/assets/2323.jpeg");
-  background-position: center;
-  background-size: cover;
-  // padding: 30px;
-  // height: 420px;
+const ViewContainer = styled.div`
   width: 100%;
   position: relative;
-  white-space: pre-line;
-  background-color: salmon;
 
   &:after {
     content: "";
     display: block;
     padding-bottom: 100%;
   }
+`;
 
-  div {
-    position: absolute;
+const Box = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+
+  white-space: pre-line;
+  // background-color: salmon;
+`;
+
+const ScreenShotBox = styled.div`
+  width: ${(props) => (props.ratio === "ratio1" ? "100%" : "70%")};
+  height: 100%;
+  background-image: url("/assets/background1.jpg");
+  background-position: center;
+  background-size: cover;
+
+  .imageSize {
     width: 100%;
     height: 100%;
+    object-fit: cover;
+  }
+`;
 
-    .imageSize {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
+const EditContainer = styled.div`
+  background-color: white;
+
+  ul {
+    display: flex;
+    align-items: center;
+
+    li {
+      width: 50px;
+      height: 50px;
+      background-color: #d9d9d9;
+      margin-left: 10px;
+      border-radius: 8px;
     }
   }
 `;
