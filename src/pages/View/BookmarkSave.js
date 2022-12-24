88621e575f8a1244cb4c22d365ce25ff81fe1e4c
cocacollllla <cocacollllla@gmail.com>
2022-12-24 20:28:44 +0900

    최신껄로 정리

--- src/pages/View/BookmarkSave.js
+++ src/pages/View/BookmarkSave.js
@@ -6,74 +6,42 @@ import { MdClose, MdOutlineSaveAlt } from "react-icons/md";
 import styled from "styled-components";
 import BookmarkSaveTab from "./BookmarkSaveTab";
 import Loading from "../../components/Loading";
-const tabMenu = ["크기", "배경", "폰트", "텍스트색상"];
+import { authorSlice } from "../../util/util";
+
+const tabMenu = ["크기", "배경", "텍스트색상"];
+
 const BookmarkSave = ({ bookmarkSaveClose, bookmark, title, author }) => {
   const [loading, setLoading] = useState(false);
   const [ratio, setRatio] = useState("square");
   const [tab, setTab] = useState(tabMenu[0]);
   const [bgImg, setBgImg] = useState("/assets/background1.jpg");
-  const [font, setFont] = useState("Noto Sans KR");
   const [textColor, setTextColor] = useState("black");
   const [isDefault, setIsDefault] = useState(true);
-
-  const [errorText, setErrorText] = useState("오류아님");
   const printRef = useRef();
   const textRef = useRef();
   const viewRef = useRef();
   const isIphone = /iPhone/i.test(navigator.userAgent);
-  // useEffect(() => {
-  //   const getList = async () => {
-  //     setLoading(true);
-  //     await textImageHandler();
-  //     setLoading(false);
-  //   };
-  //   getList();
-  // }, []);
 
-  // const textImageHandler = async () => {
-  //   const element = textRef.current;
-  //   await toPng(element).then((dataUrl) => {
-  //     setTextImage(dataUrl);
-  //   });
-  // };
   const handleDownloadImage = async () => {
     const element = printRef.current;
-    // setLoading(true);
+    setLoading(true);
     if (isDefault) {
-      setErrorText("다운시작");
       const canvas = await html2canvas(element);
       const data = canvas.toDataURL("image/png");
       const link = document.createElement("a");
-      setErrorText("a로 만듦");
+
       link.href = data;
       link.download = "image.png";
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-      setErrorText("다운완료");
     }
-    /////////////////////////////////
+
     if (!isDefault) {
-      // const url = await toPng(element);
-      // let img = document.createElement("img");
-      // img.src = url;
-      // const image = await new Promise((resolve) => {
-      //   img.onload = () => {
-      //     toPng(element).then((dataUrl) => {
-      //       resolve(dataUrl);
-      //     });
-      //   };
-      // });
-      // let link = document.createElement("a");
-      // link.download = "my-image-name.png";
-      // link.href = image;
-      // link.click();
-      setErrorText("다운시작");
       if (isIphone) {
         await toPng(element);
         await toPng(element);
       }
-
       await toPng(element)
         .then((dataUrl) => {
           const link = document.createElement("a");
@@ -82,40 +50,12 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark, title, author }) => {
           document.body.appendChild(link);
           link.click();
           document.body.removeChild(link);
-          setErrorText("다운완료");
         })
         .catch((error) => {
-          setErrorText("에러");
+          console.log(error);
         });
-      setErrorText("끝");
-      setLoading(false);
     }
-    //////////////////////////////////////
-    // const url = await toPng(element);
-    // let img = document.createElement("img");
-    // img.src = url;
-    // const image = await new Promise((resolve) => {
-    //   img.onload = () => {
-    //     toPng(element).then((dataUrl) => {
-    //       resolve(dataUrl);
-    //     });
-    //   };
-    // });
-    // let link = document.createElement("a");
-    // link.download = "my-image-name.png";
-    // link.href = image;
-    // link.click();
-    //////////////////////////////////
-    // toPng(element)
-    //   .then(function (dataUrl) {
-    //     let link = document.createElement("a");
-    //     link.download = "my-image-name.png";
-    //     link.href = dataUrl;
-    //     link.click();
-    //   })
-    //   .catch(function (error) {
-    //     console.error("oops, something went wrong!", error);
-    //   });
+    setLoading(false);
   };
   const selectBackgroundImg = (img) => {
     setBgImg(img);
@@ -123,9 +63,6 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark, title, author }) => {
   };
   const selectRatio = (ratio) => {
     setRatio(ratio);
-  };
-  const selectFont = (ft) => {
-    setFont(ft);
   };
   const selectTextColor = (color) => {
     setTextColor(color);
@@ -147,76 +84,60 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark, title, author }) => {
     };
   };
 
+  console.log(loading);
+
   return (
     <Container>
+      {loading && (
+        <div className="loading">
+          <Loading />
+        </div>
+      )}
+
       <ButtonContainer>
         <div onClick={bookmarkSaveClose}>
           <MdClose />
         </div>
-        {/* <div onClick={textImageHandler}>#</div> */}
         <div onClick={handleDownloadImage}>
           <MdOutlineSaveAlt />
         </div>
       </ButtonContainer>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          {/* <TextImageComP>
-            {textImage === "" && (
+
+      <ViewContainer ratio={ratio} textColor={textColor} ref={viewRef}>
+        <Box>
+          {!isDefault && (
+            <ScreenShotBox ref={printRef} ratio={ratio} bgImg={bgImg}>
               <div className="contentBox">
                 <div className="content" ref={textRef}>
                   <p>{bookmark.text}</p>
                   <div className="titleBox">
                     <div className="title">{title}</div>
-                    <div>{author}</div>
+                    <div className="author">{authorSlice(author)}</div>
                   </div>
                 </div>
               </div>
-            )}
-          </TextImageComP> */}
-          <ViewContainer
-            ratio={ratio}
-            textColor={textColor}
-            font={font}
-            ref={viewRef}
-          >
-            <Box>
-              <div style={{ position: "absolute" }}>{errorText}</div>
-              {!isDefault && (
-                <ScreenShotBox ref={printRef} bgImg={bgImg}>
-                  <div className="contentBox">
-                    <div className="content" ref={textRef}>
-                      <p>{bookmark.text}</p>
-                      <div className="titleBox">
-                        <div className="title">{title}</div>
-                        <div>{author}</div>
-                      </div>
-                    </div>
+            </ScreenShotBox>
+          )}
+          {isDefault && (
+            <ScreenShotBox2 ref={printRef} ratio={ratio}>
+              <img src={bgImg} alt="이미지" className="imageSize" />
+              <div className="contentBox">
+                <div className="content" ref={textRef}>
+                  <p>{bookmark.text}</p>
+                  <div className="titleBox">
+                    <div className="title">{title}</div>
+                    <div className="author">{authorSlice(author)}</div>
                   </div>
-                </ScreenShotBox>
-              )}
-              {isDefault && (
-                <ScreenShotBox2 ref={printRef}>
-                  <img src={bgImg} alt="이미지" className="imageSize" />
-                  <div className="contentBox">
-                    <div className="content" ref={textRef}>
-                      <p>{bookmark.text}</p>
-                      <div className="titleBox">
-                        <div className="title">{title}</div>
-                        <div>{author}</div>
-                      </div>
-                    </div>
-                  </div>
-                </ScreenShotBox2>
-              )}
-              {/* <div>
+                </div>
+              </div>
+            </ScreenShotBox2>
+          )}
+          {/* <div>
             <img src="/assets/2323.jpeg" alt="이미지" className="imageSize" />
           </div> */}
-            </Box>
-          </ViewContainer>
-        </>
-      )}
+        </Box>
+      </ViewContainer>
+
       <EditContainer>
         <div className="tabMenuContainer">
           {tabMenu.map((menu) => (
@@ -234,10 +155,8 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark, title, author }) => {
           selectBackgroundImg={selectBackgroundImg}
           selectRatio={selectRatio}
           selectTextColor={selectTextColor}
-          selectFont={selectFont}
           ratio={ratio}
           bgImg={bgImg}
-          font={font}
           textColor={textColor}
           onChangeBgImgHandler={onChangeBgImgHandler}
         />
@@ -246,6 +165,7 @@ const BookmarkSave = ({ bookmarkSaveClose, bookmark, title, author }) => {
   );
 };
 export default BookmarkSave;
+
 const Container = styled.div`
   position: absolute;
   top: 0;
@@ -257,20 +177,31 @@ const Container = styled.div`
   justify-content: space-between;
   flex-direction: column;
   background-color: ${(props) => props.theme.black};
+
+  .loading {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 9;
+    background: rgba(0, 0, 0, 0.4);
+  }
 `;
+
 const ViewContainer = styled.div`
   width: 100%;
   position: relative;
   background-color: #666;
   color: ${(props) => (props.textColor === "black" ? "black" : "white")};
-  font-family: ${(props) =>
-    props.font === "Noto Sans KR" ? "Noto Sans KR" : "RIDIBatang"};
+  font-family: ${(props) => props.font};
   &:after {
     content: "";
     display: block;
     padding-bottom: ${(props) => (props.ratio === "square" ? "100%" : "130%")};
   }
 `;
+
 const Box = styled.div`
   position: absolute;
   width: 100%;
@@ -278,33 +209,8 @@ const Box = styled.div`
   display: flex;
   justify-content: center;
   white-space: pre-line;
-  // background-color: salmon;
 `;
-// const TextImageComP = styled.div`
-//   position: absolute;
-//   top: 0;
-//   z-index: 999999999;
-//   height: 100%;
-//   white-space: pre-line;
-//   .contentBox {
-//     height: 100%;
-//     display: flex;
-//     justify-content: center;
-//     align-items: center;
-//     padding: 10%;
-//     font-size: 0.9rem;
-//     .content {
-//       line-height: 21px;
-//     }
-//     .titleBox {
-//       margin-top: 20px;
-//       font-size: 0.9rem;
-//       .title {
-//         font-weight: 500;
-//       }
-//     }
-//   }
-// `;
+
 const ScreenShotBox = styled.div`
   width: 100%;
   height: 100%;
@@ -312,12 +218,8 @@ const ScreenShotBox = styled.div`
   background-position: center;
   background-size: cover;
   .contentBox {
-    position: absolute;
-    top: 0;
     height: 100%;
     display: flex;
-    justify-content: center;
-
     align-items: center;
     padding: 5% 10%;
     font-size: 0.9rem;
@@ -325,21 +227,26 @@ const ScreenShotBox = styled.div`
       line-height: 21px;
       p {
         display: -webkit-box;
-        -webkit-line-clamp: 13;
+        -webkit-line-clamp: ${(props) =>
+          props.ratio === "square" ? "13" : "18"};
         -webkit-box-orient: vertical;
         overflow: hidden;
         text-overflow: clip;
       }
     }
     .titleBox {
-      margin-top: 20px;
+      margin-top: 30px;
       font-size: 0.9rem;
       .title {
         font-weight: 500;
       }
+      .author {
+        font-size: 0.8rem;
+      }
     }
   }
 `;
+
 const ScreenShotBox2 = styled.div`
   width: 100%;
   height: 100%;
@@ -353,7 +260,6 @@ const ScreenShotBox2 = styled.div`
     top: 0;
     height: 100%;
     display: flex;
-    justify-content: center;
     align-items: center;
     padding: 5% 10%;
     font-size: 0.9rem;
@@ -361,21 +267,26 @@ const ScreenShotBox2 = styled.div`
       line-height: 21px;
       p {
         display: -webkit-box;
-        -webkit-line-clamp: 13;
+        -webkit-line-clamp: ${(props) =>
+          props.ratio === "square" ? "13" : "18"};
         -webkit-box-orient: vertical;
         overflow: hidden;
         text-overflow: clip;
       }
     }
     .titleBox {
-      margin-top: 20px;
+      margin-top: 30px;
       font-size: 0.9rem;
       .title {
         font-weight: 500;
       }
+      .author {
+        font-size: 0.8rem;
+      }
     }
   }
 `;
+
 const EditContainer = styled.div`
   padding: 0 5px;
   .tabMenuContainer {
@@ -393,6 +304,7 @@ const EditContainer = styled.div`
     }
   }
 `;
+
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: space-between;
